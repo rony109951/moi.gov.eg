0caad77e3cef695eb05a4468c47d72e43204f5c1
rony109951 <mohmd153205@gmail.com>
2025-09-03 02:31:47 +0300

    Update script.js
    H

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function writeText() {
   let text = document.getElementById( userText ).value;
-  document.getElementById( notebookText ).innerText = text;
+  let notebook = document.getElementById( notebookText );
+  
+  // إظهار النص داخل الكراسة مع تنسيق الأسطر
+  notebook.innerText = text;
+  
+  // التمرير إلى أسفل الصفحة لعرض الكراسة
+  window.scrollTo(0, document.body.scrollHeight);
 }
